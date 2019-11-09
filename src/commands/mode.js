c0cd8304dafc00442fe91f05ef10966715861e3d
pozil <philippe.ozil@gmail.com>
2019-11-09 15:50:54 +0100

    Merged mode commands

--- src/commands/mode.js
+++ src/commands/mode.js
@@ -7,19 +7,27 @@ class ModeSetCommand extends Command {
         const { mode } = flags;
 
         const bot = await getUltimateBot();
-        bot.setMode(mode);
-        this.log(`MegaPi new mode: ${mode}.`);
+
+        if (mode === undefined) {
+            const readMode = await bot.getMode();
+            this.log(`MegaPi mode: ${readMode}`);
+        } else {
+            bot.setMode(mode);
+            this.log(`New MegaPi mode: ${mode}`);
+        }
+
         await bot.disconnect();
     }
 }
 
-ModeSetCommand.description = 'Sets the MegaPi mode';
+ModeSetCommand.description = 'Gets or sets the MegaPi mode';
 
 ModeSetCommand.flags = {
     mode: flags.integer({
         char: 'm',
         description:
-            'New mode: 0 = manual mode, 1 = obstacle avoidance, 2 = balance, 3 = IR remote, 4 = line following'
+            'New mode: 0 = manual mode, 1 = obstacle avoidance, 2 = balance, 3 = IR remote, 4 = line following',
+        options: ['0', '1', '2', '3', '4']
     })
 };
 
